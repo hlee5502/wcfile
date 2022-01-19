40bdf1d7c3b5efd3063423bc3eb12859ea9b274d
Haejin <60941664+hlee5502@users.noreply.github.com>
2022-01-19 03:37:30 -0800

    Update wcfile.h

--- wcfile.h
+++ wcfile.h
@@ -4,23 +4,11 @@
 #ifndef CH_HEAD
 #define CH_HEAD
 
-// solve function reasd equations from a file and writes them in a solved state to another file
+// solve function reads equations from a file and writes them in a solved state to another file
 
 // param qfile: file with math questions to solve
 // param afile: file to write equations with answers in
-// example:
-//   qfile at start of function:
-//     12 + 13
-//     24 / 5
-//     8 * 234
-//     65 - 78
-//     239 % 13
-//   afile after function:
-//      12 +  13 = 25
-//      24 /   5 = 4
-//       8 * 234 = 1872
-//      65 -  78 = -13
-//     239 %  13 = 5
+
 void solve(const char* qfile, const char* afile);
 
 #endif
